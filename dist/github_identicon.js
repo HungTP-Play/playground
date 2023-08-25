0698df30531136e2a30404cbcd4285401da89754
Hung Tran <hung.p.tran@opswat.com>
2023-08-25 20:26:11 +0700

    build: identicon

--- dist/github_identicon.js
+++ dist/github_identicon.js
@@ -11,23 +11,66 @@ $parcel$export(module.exports, "GithubIdenticon", () => $80edcdc4c527ecf0$export
  * [CryptoJS](https://www.npmjs.com/package/crypto-js)
  */ 
 class $80edcdc4c527ecf0$export$f7c98094a623f7aa {
-    constructor(value){}
+    value = "";
+    constructor(value){
+        this.value = value;
+    }
     calculateHash(value) {
         return $1HOpm$cryptojs.SHA256(value);
     }
     hashHex(hashVal) {
         return hashVal.toString($1HOpm$cryptojs.enc.Hex);
     }
-    hashBin(hashVal) {
+    hashBin(hex) {
         var binary = "";
-        const hex = this.hashHex(hashVal);
-        for(var i = 0; i < hex.length; i += 2){
-            var hexByte = hex.substr(i, 2);
+        for(var i1 = 0; i1 < hex.length; i1 += 2){
+            var hexByte = hex.substr(i1, 2);
             var byteValue = parseInt(hexByte, 16);
             var byteBinary = byteValue.toString(2).padStart(8, "0");
             binary += byteBinary;
         }
         return binary;
+    }
+    binaryToInt(binString) {
+        return parseInt(binString, 2);
+    }
+    getBits(from, len, binString) {
+        return binString.slice(from, from + len);
+    }
+    getRgbColor(binString) {
+        const redBits = this.getBits(0, 8, binString);
+        const greenBits = this.getBits(8, 8, binString);
+        const blueBits = this.getBits(16, 8, binString);
+        const redVal = this.binaryToInt(redBits);
+        const greenVal = this.binaryToInt(greenBits);
+        const blueVal = this.binaryToInt(blueBits);
+        return [
+            redVal,
+            greenVal,
+            blueVal
+        ];
+    }
+    getPlacement(binString) {
+        const START_OFFSET = 24;
+        const placementFromBits = new Array(15).fill(0).map((_, i1)=>{
+            return START_OFFSET + i1 * 8;
+        });
+        const placementValues = placementFromBits.map((v)=>{
+            const bits = this.getBits(i, 8, binString);
+            return this.binaryToInt(bits);
+        });
+        return placementValues.map((v)=>v % 2 == 0);
+    }
+    getIdenticonParts() {
+        const valHash = this.calculateHash(this.value);
+        const valHashHex = this.hashHex(valHash);
+        const valHasBin = this.hashBin(valHashHex);
+        const rgb = this.getRgbColor(valHasBin);
+        const placements = this.getPlacement(valHasBin);
+        return {
+            rgb: rgb,
+            placements: placements
+        };
     }
 }
 
--- github_identicon.js
+++ github_identicon.js
@@ -5,7 +5,10 @@
  */
 import * as Crypto from 'crypto-js';
 export class GithubIdenticon {
-    constructor(value) { }
+    value = ''
+    constructor(value) {
+        this.value = value;
+    }
 
     calculateHash(value) {
         return Crypto.SHA256(value);
@@ -16,9 +19,8 @@ export class GithubIdenticon {
     }
 
 
-    hashBin(hashVal) {
+    hashBin(hex) {
         var binary = "";
-        const hex = this.hashHex(hashVal);
         for (var i = 0; i < hex.length; i += 2) {
             var hexByte = hex.substr(i, 2);
             var byteValue = parseInt(hexByte, 16);
@@ -26,5 +28,53 @@ export class GithubIdenticon {
             binary += byteBinary;
         }
         return binary;
+    }
+
+    binaryToInt(binString) {
+        return parseInt(binString, 2);
+    }
+
+    getBits(from, len, binString) {
+        return binString.slice(from, from + len)
+    }
+
+    getRgbColor(binString) {
+        const redBits = this.getBits(0, 8, binString);
+        const greenBits = this.getBits(8, 8, binString);
+        const blueBits = this.getBits(16, 8, binString);
+
+        const redVal = this.binaryToInt(redBits);
+        const greenVal = this.binaryToInt(greenBits);
+        const blueVal = this.binaryToInt(blueBits);
+
+        return [redVal, greenVal, blueVal];
+    }
+
+    getPlacement(binString) {
+        const START_OFFSET = 24
+        const placementFromBits = new Array(15).fill(0).map((_, i) => {
+            return START_OFFSET + i * 8;
+        });
+
+        const placementValues = placementFromBits.map((v) => {
+            const bits = this.getBits(i, 8, binString);
+            return this.binaryToInt(bits)
+        });
+
+        return placementValues.map((v) => v % 2 == 0);
+    }
+
+    getIdenticonParts() {
+        const valHash = this.calculateHash(this.value);
+        const valHashHex = this.hashHex(valHash);
+        const valHasBin = this.hashBin(valHashHex);
+
+        const rgb = this.getRgbColor(valHasBin);
+        const placements = this.getPlacement(valHasBin);
+
+        return {
+            rgb,
+            placements,
+        }
     }
 }
